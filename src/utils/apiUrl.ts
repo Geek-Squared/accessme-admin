969549a29e51750da1ff9e5e1e155068e98e1760
Moses Ngwerume <moses.ngwerume@wyzetalk.com>
2024-11-28 09:25:52 +0200

    added api url

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -1,1 +1,4 @@
-export const apiUrl = import.meta.env.VITE_API_URL;
+export const apiUrl = 
+  process.env.NODE_ENV === 'production'
+    ? 'https://acsys-production.up.railway.app'
+    : import.meta.env.VITE_API_URL;
